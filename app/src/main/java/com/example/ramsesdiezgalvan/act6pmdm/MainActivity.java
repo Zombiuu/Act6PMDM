cd2fab32651e5714f020d90444ebdf7c6d0f0d53
Zombiuu <ramses.diez@live.u-tad.com>
2018-01-30 11:58:21 +0100

    Twitter acabado, funciona y saca el nombre de usuario. Tambien registra las cuentas tanto de FaceBook como de Twitter en FireBase

--- app/src/main/java/com/example/ramsesdiezgalvan/act6pmdm/MainActivity.java
+++ app/src/main/java/com/example/ramsesdiezgalvan/act6pmdm/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ramsesdiezgalvan.act6pmdm;
 
 import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +18,14 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.TwitterAuthProvider;
 import com.squareup.picasso.Picasso;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
@@ -32,6 +42,8 @@ import org.json.JSONObject;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
     CallbackManager callbackManager;
     LoginButton loginButton_fb;
     TwitterLoginButton loginButton_tw;
@@ -47,13 +59,19 @@ public class MainActivity extends AppCompatActivity {
         Twitter.initialize(this);
         setContentView(R.layout.activity_main);
         callbackManager = CallbackManager.Factory.create();
-
+        firebaseAuth = FirebaseAuth.getInstance();
         //TWITTER
         loginButton_tw = (TwitterLoginButton) findViewById(R.id.login_button_tw);
+
+
+
+
+
 
         loginButton_tw.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
+                handleTwitterSession(result.data);
                 // Do something with result, which provides a TwitterSession for making API calls
                 Log.v("twitter", "Estoy mas dentro");
                 Log.v("twitter", "RESULT: " + result);
@@ -63,12 +81,10 @@ public class MainActivity extends AppCompatActivity {
 
                 String token = authToken.token;
                 String secret = authToken.secret;
-                Log.v("twitter", "TOKEN: "+token);
-                Log.v("twitter", "SECRET: "+secret);
+                Log.v("twitter", "TOKEN: " + token);
+                Log.v("twitter", "SECRET: " + secret);
 
-                name.setText(session.getUserName().toString());
-
-
+                name.setText("@"+session.getUserName().toString());
 
             }
 
@@ -94,16 +110,20 @@ public class MainActivity extends AppCompatActivity {
         loginButton_fb.setReadPermissions(Arrays.asList
                 ("public_profile", "user_friends", "email", "user_photos", "  user_birthday", "read_custom_friendlists"));
 
+
         // Callback registration
         loginButton_fb.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                // App code
+
 
                 System.out.println("ESTOY MAS DENTRO");
 
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
                 GraphRequest request = GraphRequest.newMeRequest(
                         AccessToken.getCurrentAccessToken(),
+
                         new GraphRequest.GraphJSONObjectCallback() {
 
 
@@ -170,6 +190,66 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.v("TOKEN","Esto es el token ---->"+token);
+        Log.v("TOKEN","Esto es el token string ---->"+token.toString());
+        Log.v("TOKEN","Esto es el token getToken ---->"+token.getToken());
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("", "signInWithCredential:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void handleTwitterSession(TwitterSession session) {
+        Log.d("", "handleTwitterSession:" + session);
+        Log.v("twitter pass","1: "+ session.getAuthToken().token);
+        Log.v("twitter pass","2: "+ session.getAuthToken().secret);
+        AuthCredential credential = TwitterAuthProvider.getCredential(
+                session.getAuthToken().token,
+                session.getAuthToken().secret);
+
+
+
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("", "signInWithCredential:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 
     //Twitter
 //    @Override
